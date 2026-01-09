882672cf6ba09750ab9127a2e2f9ca077b912d4e
unknown <molanddise@gmail.com>
2026-01-09 15:18:32 -0600

    The Directive colors were created and inputs were added to dynamically inject them into the componets

---
+++ src/app/pages/board/boards-component/boards-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from "../../../components/navbar/navbar-component/navbar-component";
+
+@Component({
+  selector: 'app-boards-component',
+  imports: [NavbarComponent],
+  templateUrl: './boards-component.html',
+})
+export class BoardsComponent {
+
+}
--- src/app/pages/login-componet/login-componet.ts
+++ src/app/pages/login-componet/login-componet.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+import { BtnComponent } from "../../components/btn-component/btn-component";
 
 @Component({
   selector: 'app-login-componet',
-  imports: [],
+  imports: [RouterLink, BtnComponent],
   templateUrl: './login-componet.html',
 })
 export class LoginComponet {
