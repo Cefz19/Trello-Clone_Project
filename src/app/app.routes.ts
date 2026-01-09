51e2c67040142940aab09c20bbb66a8b6a8f9fc6
unknown <molanddise@gmail.com>
2026-01-08 18:01:08 -0600

    The layout was forms with title, input,and button

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponet } from './pages/login-componet/login-componet';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login', 
+        component: LoginComponet,
+    }
+];
---
+++ src/app/pages/login-componet/login-componet.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-componet',
+  imports: [],
+  templateUrl: './login-componet.html',
+})
+export class LoginComponet {
+
+}
