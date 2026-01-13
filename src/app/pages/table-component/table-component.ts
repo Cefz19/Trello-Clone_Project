b26d464a959fe2257de8264525319820fc4b74e0
unknown <molanddise@gmail.com>
2026-01-13 17:54:53 -0600

    The cdk directive collection module was imported from Material.Angular.io, this module provide context for managing the dataSource. Similarly, the data-source.ts file was created in Angular to add methods and implement them in the tables.

--- src/app/pages/table-component/table-component.ts
+++ src/app/pages/table-component/table-component.ts
@@ -1,24 +1,35 @@
 import { Component, ChangeDetectorRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CdkTableModule } from '@angular/cdk/table';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { NavbarComponent } from "../../components/navbar-component/navbar-component";
+import { BtnComponent } from "../../components/btn-component/btn-component";
+
 import { HttpClient } from '@angular/common/http';
+import { DataSourceProduct } from './data-source';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-table-component',
   imports: [
     CdkTableModule,
     CommonModule,
+    ReactiveFormsModule,
     NavbarComponent,
+    BtnComponent
 ],
   templateUrl: './table-component.html',
 })
 export class TableComponent {
   private cd = inject(ChangeDetectorRef);
 
-  products: Product[] = [];
-  columns: string[] = ['id', 'title', 'price', 'cover'];
+  // products: Product[] = [];
+  dataSource = new DataSourceProduct();
+  columns: string[] = ['id', 'title', 'price', 'cover', 'actions'];
   total = 0;
+  input = new FormControl('', { nonNullable:  true })
 
   constructor(
     private _http: HttpClient
@@ -27,12 +38,31 @@ export class TableComponent {
   ngOnInit(){
     this._http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
     .subscribe(data => {
-      this.products = data;
-      this.total = this.products
-      .map(item => item.price)
-      .reduce((price, total) => price + total, 0);
-      this.cd.detectChanges()
+      this.dataSource.init(data);
+      this.total = this.dataSource.getTotal()
+    })
+    this.input.valueChanges
+    .pipe(
+      debounceTime(300)
+    )
+    .subscribe(value => {
+      this.dataSource.find(value);
     })
   }
+
+    update(product: Product) {
+      this.dataSource.update(product.id, { price: 20 })
+    }
+
+  // ngOnInit(){
+  //   this._http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+  //   .subscribe(data => {
+  //     this.products = data;
+  //     this.total = this.products
+  //     .map(item => item.price)
+  //     .reduce((price, total) => price + total, 0);
+  //     this.cd.detectChanges()
+  //   })
+  // }
 
 }
